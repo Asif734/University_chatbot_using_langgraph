e31dfd04fa36e4c190dfecb07898f8c5b4069309
Asifuzzaman Asif <asifuzzamanasif734@gmail.com>
2025-11-06 05:48:08 +0600

    fronted link added

--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI 
 from app.routes import upload_file, query, authentication
+from fastapi.middleware.cors import CORSMiddleware
 
 app= FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://bup-chat.vercel.app", "http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 
 @app.get("/")
 async def get_root():
